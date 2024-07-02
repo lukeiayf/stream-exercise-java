ec7698090089b22997351e0cf4ac87761fd5fa88
Lucas Gomes <lukas_g_s_@hotmail.com>
2024-07-02 17:07:03 -0300

    adds stream logic

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,10 +5,7 @@ import entities.Product;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,16 +13,25 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter full file path: ");
         String path = sc.nextLine();
-
+        //file formatting is -> name,price
         try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             List<Product> list = new ArrayList<>();
             String line = br.readLine();
             while (line != null) {
-                String[] fields = line.split(" ");
+                String[] fields = line.split(",");
                 list.add(new Product(fields[0], Double.parseDouble(fields[1])));
                 line = br.readLine();
             }
+
+            double avg = list.stream().map(Product::getPrice).reduce(0.0, Double::sum) / list.size();
+            System.out.println("Average price: " + String.format("%.2f", avg));
+
+            Comparator<String> comp = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+            List<String> names = list.stream().filter(p -> p.getPrice() < avg).map(Product::getName).sorted(comp.reversed()).toList();
+
+            names.forEach(System.out::println);
+
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
         }
